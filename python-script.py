4f6fc08f9f41210637b2a833d9f6135757de0222
Chavez Harris <codedbychavez@gmail.com>
2023-12-17 11:10:03 -0400

    Updates

--- python-script.py
+++ python-script.py
@@ -6,14 +6,25 @@ def main():
     # Load the kube config file
     config.load_kube_config()
 
-    file = path.join(path.dirname(__file__), "pod-b.yaml")
+    kube_deployment = "deployment-a.yaml"
+    kube_service = "service.yaml"
 
-    with open(file) as f:
+    deployment_file = path.join(path.dirname(__file__), kube_deployment)
+    service_file = path.join(path.dirname(__file__), kube_service)
+
+    with open(deployment_file) as f:
         dep = yaml.safe_load(f)
         k8s_apps_v1 = client.AppsV1Api()
         resp = k8s_apps_v1.create_namespaced_deployment(
             body=dep, namespace="default")
-        print("Pod A deployment created. status='%s'" % str(resp.status))
+        print("Deployment created. status='%s'" % str(resp.status))
+
+    with open(service_file) as f:
+        dep = yaml.safe_load(f)
+        k8s_core_v1 = client.CoreV1Api()
+        resp = k8s_core_v1.create_namespaced_service(
+            body=dep, namespace="default")
+        print("Service created. status='%s'" % str(resp.status))
 
 if __name__ == '__main__':
     main()
