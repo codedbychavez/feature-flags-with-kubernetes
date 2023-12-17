1f67495ab046c2ad954030541da72bb7501a35f0
Chavez Harris <codedbychavez@gmail.com>
2023-12-17 12:01:21 -0400

    Updates

--- python-script.py
+++ python-script.py
@@ -6,18 +6,45 @@ def main():
     # Load the kube config file
     config.load_kube_config()
 
-    kube_deployment = "deployment-a.yaml"
-    kube_service = "service.yaml"
+    deployment_file = path.join(path.dirname(__file__), "deployment.yaml")
+    service_file = path.join(path.dirname(__file__), "service.yaml")
 
-    deployment_file = path.join(path.dirname(__file__), kube_deployment)
-    service_file = path.join(path.dirname(__file__), kube_service)
+    # Check if the feature flag is enabled
 
+    is_enabled = True
+    
     with open(deployment_file) as f:
         dep = yaml.safe_load(f)
         k8s_apps_v1 = client.AppsV1Api()
         resp = k8s_apps_v1.create_namespaced_deployment(
-            body=dep, namespace="default")
+        body=dep, namespace="default")
         print("Deployment created. status='%s'" % str(resp.status))
+
+        if is_enabled:
+            # Define the image
+            image = "backend-with-node20"
+
+            # Define the patch
+            patch = {
+                "spec": {
+                    "template": {
+                        "spec": {
+                            "containers": [
+                                {
+                                    "name": "container_name",
+                                    "image": image
+                                }
+                            ]
+                        }
+                    }
+                }
+            }
+            
+            resp = k8s_apps_v1.patch_namespaced_deployment(
+                name="backend-deployment",
+                namespace="default",
+                body=patch,
+            )
 
     with open(service_file) as f:
         dep = yaml.safe_load(f)
