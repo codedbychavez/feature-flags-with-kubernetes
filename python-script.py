ec59679bac11a01b8b1f07201111d3e01bbc5ce9
Chavez Harris <codedbychavez@gmail.com>
2023-12-16 20:43:43 -0400

    Updates

--- python-script.py
+++ python-script.py
@@ -1,25 +1,19 @@
+from kubernetes import client, config
 from os import path
-
 import yaml
 
-from kubernetes import client, config
-
 def main():
-    # Configs can be set in Configuration class directly or using 
-    # helper utility. If no argument provided, the config will be loaded from 
-    # default location.
+    # Load the kube config file
     config.load_kube_config()
 
-    file = path.join(path.dirname(__file__), "pod-one.yaml")
+    file = path.join(path.dirname(__file__), "pod-b.yaml")
 
     with open(file) as f:
         dep = yaml.safe_load(f)
-        k8s_beta = client.ExtensionsV1beta1Api()
-        resp = k8s_beta.create_namespaced_deployment(
+        k8s_apps_v1 = client.AppsV1Api()
+        resp = k8s_apps_v1.create_namespaced_deployment(
             body=dep, namespace="default")
-        print("Deployment created. status='%s'" % str(resp.status))
+        print("Pod A deployment created. status='%s'" % str(resp.status))
 
 if __name__ == '__main__':
     main()
-
-    
